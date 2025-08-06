32309c884f2743201f90673916a68ada239c8120
Daniel Mariano <daniel_aam@icloud.com>
2025-08-06 09:30:16 -0400

    feat: arrumar app module acrescentar o que faltava

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,15 @@ import { OcorrenciaModule } from './modules/ocorrencia/ocorrencia.module';
 import { HistoricoModule } from './modules/historico/historico.module';
 import { CompraInsumoModule } from './modules/compra-insumo/compra-insumo.module';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { InvernadaModule } from './modules/invernada/invernada.module';
+import { FinanceiroModule } from './modules/financeiro/financeiro.module';
+import { LavouraModule } from './modules/lavoura/lavoura.module';
+import { LeituraDispositivoModule } from './modules/leitura-dispositivo/leitura-dispositivo.module';
+import { ManejoModule } from './modules/manejo/manejo.module';
+import { MarcaModeloModule } from './modules/modelo-dispositivo/marca-modelo.module';
+import { PesagemModule } from './modules/pesagem/pesagem.module';
+import { SanidadeModule } from './modules/sanidade/sanidade.module';
+
 
 @Module({
   imports: [
@@ -22,13 +31,21 @@ import { DashboardModule } from './modules/dashboard/dashboard.module';
     }),
     // ...existing code...
     UsuarioModule,
+    SanidadeModule,
+    PesagemModule,
+    MarcaModeloModule,
     FazendaModule,
+    ManejoModule,
+    LeituraDispositivoModule,
+    LavouraModule,
     DashboardModule,
     OcorrenciaModule,
     MedicamentoModule,
     AnimalModule,
     HistoricoModule,
     CompraInsumoModule,
+    InvernadaModule,
+    FinanceiroModule,
     AuthModule,
     JwtModule.register({}),
     MailerModule.forRootAsync({
